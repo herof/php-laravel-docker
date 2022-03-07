cfa553b0a481db389e4ccca98875d4b89613ba08
Hiro <37823056+herof@users.noreply.github.com>
2022-03-07 15:30:51 +0900

    Add Insert Data to users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('users')->insert([
+            'name' => 'test1',
+            'email' => 'test1@test.com',
+            'password' => bcrypt('test1') 
+        ]);
+
     }
 }
